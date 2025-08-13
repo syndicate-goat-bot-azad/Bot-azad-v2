4c3eb7f73e0eef6549f1f9ff25b50868388da617
syndicate-goat-bot-azad <talukdershoikat@gmail.com>
2025-08-13 20:16:25 +0600

    Update Azadbot.js

--- scripts/cmds/Azadbot.js
+++ scripts/cmds/Azadbot.js
@@ -1,24 +1,28 @@
-module.exports = {
- config: {
-	 name: "Azad bot",
-	 version: "1.0",
-	 author: "Hasan",//remodified by cliff
-	 countDown: 5,
-	 role: 0,
-	 shortDescription: "no prefix",
-	 longDescription: "no prefix",
-	 category: "no prefix",
- },
+// Author: Azad
 
- onStart: async function(){}, 
- onChat: async function({ event, message, getLang }) {
- if (event.body && event.body.toLowerCase() === "Azad bot") {
- return message.reply({
- body: `
-       Hey My Name  Is Azad bot ❄️
-       𝐻𝑜𝑤 𝑐𝑎𝑛 𝑖 𝑎𝑠𝑠𝑖𝑠𝑡 𝑦𝑜𝑢 ?
-       𝑂𝑊𝑁𝐸𝑅 :https://www.facebook.com/profile.php?id=61578365162382 `
-        });
-      }
-   }
-}
+module.exports = {
+  config: {
+    name: "Azad bot",
+    version: "1.0",
+    author: "Azad", // remodified by cliff
+    countDown: 5,
+    role: 0,
+    shortDescription: "no prefix",
+    longDescription: "no prefix",
+    category: "no prefix",
+  },
+
+  onStart: async function () {},
+
+  onChat: async function ({ event, message }) {
+    if (event.body && event.body.toLowerCase() === "azad bot") {
+      return message.reply({
+        body: `
+Hey, my name is 𝐀𝐳𝐚𝐝 𝐁𝐨𝐭 ❄️
+𝐇𝐨𝐰 𝐜𝐚𝐧 𝐈 𝐚𝐬𝐬𝐢𝐬𝐭 𝐲𝐨𝐮?
+Owner: https://www.facebook.com/profile.php?id=61578365162382
+        `,
+      });
+    }
+  },
+};
